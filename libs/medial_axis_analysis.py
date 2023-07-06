aab3c011d5781d58eb311d562765ca9b3aeb3ecc
NinV <quannl71290@gmail.com>
2023-07-06 14:13:44 +0900

    detect stem location

--- libs/medial_axis_analysis.py
+++ libs/medial_axis_analysis.py
@@ -1,8 +1,12 @@
+from typing import Optional
 import numpy as np
 import cv2
 from skimage.morphology import skeletonize, medial_axis
 from scipy.interpolate import splprep, splev
+from shapely.geometry import Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
+
 from scipy.spatial.distance import cdist
+from matplotlib import pyplot as plt
 import networkx as nx
 
 
@@ -16,74 +20,26 @@ def calc_skeleton_line(binary_mask, method='zhang'):
     else:
         raise ValueError('Method must be in {"zhang", "lee", "medial_axis"}')
 
-    coords = np.argwhere(skeleton)
+    coords = np.argwhere(skeleton)[:, ::-1]  # coords = (n, 2) = [[x0, y0], [x1, y1], ...]
     return skeleton, coords
 
 
-def graph_based_curve_length(coords):
-    # Create a graph and add nodes
-    graph = nx.Graph()
-    graph.add_nodes_from(range(len(coords)))
-
-    # Connect neighboring points based on smallest distance
-    dist_matrix = cdist(coords, coords)
-    neighborhood_radius = 3  # Adjust this parameter to define the neighborhood size
-    for i in range(len(coords)):
-        for j in range(i + 1, len(coords)):
-            dist = dist_matrix[i, j]
-            if dist <= neighborhood_radius:
-                graph.add_edge(i, j, weight=dist)
-    # Find the shortest path between two points
-    shortest_path = nx.shortest_path(graph, source=0, target=len(coords) - 1)
-
-    # Calculate the length of the shortest path in terms of pixels and Euclidean distance
-    shortest_path_length = sum(graph[u][v]['weight'] for u, v in zip(shortest_path[:-1], shortest_path[1:]))
-    return shortest_path_length
+def curve_fitting(coords, k=3, s=None):
+    if s is None:
+        s = coords.shape[0]
+    tck, _ = splprep(coords.T, k=k, s=s)
+    return tck
 
 
-def curve_fitting(coords):
-    s_values = np.linspace(0, 1, num=10)
-
-    # Calculate the average error for each 's' value
-    errors = []
-    params = []
-    for s in s_values:
-        # Perform spline fitting to get a smooth curve approximation
-        tck, _ = splprep(coords.T, s=s)
-        params.append(tck)
-
-        # Evaluate the spline at the original coordinates
-        curve_points = np.array(splev(coords[:, 0], tck)).T
-
-        # Calculate the mean squared error
-        mse = np.mean(np.square(coords - curve_points))
-        errors.append(mse)
-
-    # Find the index of the minimum error
-    best_index = np.argmin(errors)
-    best_s = s_values[best_index]
-    best_error = errors[best_index]
-    best_params = params[best_index]
-
-    return best_s, best_error, best_params
+def build_polygon(binary_mask):
+    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = max(contours, key=cv2.contourArea)
+    points = [tuple(point[0]) for point in largest_contour]
+    polygon = Polygon(points)
+    return polygon
 
 
-def arc_length_parameterization_method(coords, tck=None, num_samples=1000):
-    if tck is None:
-        _, _, tck = curve_fitting(coords)
-
-    # Evaluate the spline at equidistant parameter values
-    t = np.linspace(0, 1, num_samples)
-    curve_points = np.array(splev(t, tck)).T
-    # Calculate the length of the curve
-    curve_length = np.sum(np.linalg.norm(curve_points[1:] - curve_points[:-1], axis=1))
-    return curve_length
-
-
-def chord_length_parameterization_method(coords, tck=None, num_samples=1000):
-    if tck is None:
-        _, _, tck = curve_fitting(coords)
-
+def chord_length_parameterization_method(tck, num_samples=1000):
     # Compute the chord lengths
     t = np.linspace(0, 1, num_samples)
     curve_points = np.array(splev(t, tck)).T
@@ -102,27 +58,198 @@ def chord_length_parameterization_method(coords, tck=None, num_samples=1000):
 
     # Calculate the length of the curve
     curve_length = np.sum(np.linalg.norm(curve_points[1:] - curve_points[:-1], axis=1))
-    return curve_length
+    return curve_points, parameter_values, curve_length
 
 
-def main():
-    # Assume binary_mask is the binary mask representing the shape
-    binary_mask = cv2.imread('0.png', cv2.IMREAD_GRAYSCALE)
-
-    # Compute the skeleton
-    skeleton, coords = calc_skeleton_line(binary_mask)
-
-    curve_length_graph = graph_based_curve_length(coords)
-
-    _, _, tck = curve_fitting(coords)
-    curve_length_arc = arc_length_parameterization_method(coords, tck, 1000)
-    curve_length_chord = chord_length_parameterization_method(coords, tck, 1000)
-
-    print("Curve length (Graph based method):", curve_length_graph)
-    print("Curve length (arc method):", curve_length_arc)
-    print("Curve length (chord method):", curve_length_chord)
+def arc_length_parameterization_method(tck, num_samples=1000):
+    # Evaluate the spline at equidistant parameter values
+    t = np.linspace(0, 1, num_samples)
+    curve_points = np.array(splev(t, tck)).T
+    # Calculate the length of the curve
+    curve_length = np.sum(np.linalg.norm(curve_points[1:] - curve_points[:-1], axis=1))
+    return curve_points, t, curve_length
 
 
-if __name__ == '__main__':
-    main()
+def cal_spline_length(tck, method='chord_length', **kwargs):
+    if method == 'chord_length':
+        return chord_length_parameterization_method(tck, **kwargs)
+    elif method == 'arc_length':
+        return arc_length_parameterization_method(tck, **kwargs)
+    else:
+        raise ValueError('Not recognize method="{}"'.format(method))
 
+
+def derivative(tck, t, h=1e-5):
+    p1, p2 = np.array(splev([t - h, t + h], tck)).T
+    return (p2 - p1) / (2 * h)
+
+
+def get_intersection_coordinates(geometry):
+    if isinstance(geometry, LineString) and len(geometry.coords) == 2:
+        return geometry
+    raise ValueError
+
+
+class CucumberShape:
+    def __init__(self, skel_method='zhang', bspline_k=3, bspline_s='auto', bspline_num_points=1000):
+        self.skel_method = skel_method
+        self.bspline_k = bspline_k
+        self.bspline_s = bspline_s
+        self.bspline_num_points = bspline_num_points
+
+    def find_medial_axis(self, binary_mask):
+        skeleton, coords = calc_skeleton_line(binary_mask, self.skel_method)
+
+        if self.bspline_s == 'auto':
+            skel_tck = curve_fitting(coords, k=self.bspline_k)
+        else:
+            skel_tck = curve_fitting(coords, k=self.bspline_k, s=self.bspline_s)
+        boundary = build_polygon(binary_mask)
+
+        # extending the bspline curve to find intersection
+        t = np.linspace(-0.1, 1.1, self.bspline_num_points)
+        curve_points = splev(t, skel_tck)
+        curve_points = np.array([(x, y) for (x, y) in zip(*curve_points)])
+        curve_line = LineString(coordinates=curve_points)
+        p1, p2 = curve_line.intersection(boundary).boundary.geoms
+        intersections = np.array([[p1.x, p1.y], [p2.x, p2.y]])
+        intersect_cdist = cdist(curve_points, intersections)
+        p1_idx, p2_idx = np.argmin(intersect_cdist, axis=0)
+        curve_points[p1_idx] = p1.x, p1.y
+        curve_points[p2_idx] = p2.x, p2.y
+        curve_points = curve_points[min(p1_idx, p2_idx): max(p1_idx, p2_idx) + 1]
+        curve_tck = curve_fitting(curve_points, k=self.bspline_k)
+        return curve_tck, skeleton, coords, boundary
+
+    def width_profile(self, tck, boundary: Polygon, num_samples=100, parameterized_method='chord_length',
+                      peak_thresh=0.3, max_stem_width=0.2):
+        # boundary size
+        x_min, y_min, x_max, y_max = boundary.bounds
+        max_size = (x_max - x_min, y_max - y_min)
+
+        # b-spline curve_points and normal_vectors
+        curve_points, t, curve_length = cal_spline_length(tck, method=parameterized_method, num_samples=num_samples)
+        dx, dy = splev(t, tck, der=1)  # derivatives
+        normal_vectors = np.stack((-dy, dx), axis=1)
+        normal_vectors = normal_vectors / np.linalg.norm(normal_vectors, axis=1, keepdims=True)
+
+        # constructing normal line
+        p1s = curve_points + normal_vectors * max_size
+        p2s = curve_points - normal_vectors * max_size
+
+        normal_lines = []
+        indices = []
+        widths = []
+        for i, (p1, p2) in enumerate(zip(p1s, p2s)):
+            line = LineString([p1, p2])
+            intersections = line.intersection(boundary)
+            try:
+                width_line_segment = get_intersection_coordinates(intersections)
+            except ValueError:
+                continue
+            # p1, p2 = coords
+            normal_lines.append(width_line_segment)
+            widths.append(normal_lines[-1].length)
+            indices.append(i)
+
+        widths = np.array(widths)
+        width_median = np.median(widths)
+
+        if len(indices) != curve_points.shape[0]:
+            curve_points = curve_points[indices]
+            normal_vectors = normal_vectors[indices]
+            t = t[indices]
+
+        chord_lengths = np.linalg.norm(curve_points[1:] - curve_points[:-1], axis=1)
+        # curve_length = np.sum(np.linalg.norm(curve_points[1:] - curve_points[:-1], axis=1))
+        cumulative_lengths = np.concatenate(([0], np.cumsum(chord_lengths)))
+        abs_change_rate = np.zeros(widths.shape[0])
+        abs_change_rate[1:] = np.abs(widths[1:] - widths[0:-1])
+
+        # finding stem region
+        # assume stem is from first half (important)
+        n = widths.shape[0]
+        peak_idx = np.argmax(abs_change_rate[:n//2])
+        if abs_change_rate[peak_idx] > peak_thresh * width_median:      # threshold test for long and narrow stem
+            stem_idx = peak_idx
+        else:                                                           # short and thick stem
+            # find the first idx where width less than max_stem_width
+            stem_idx = np.argmin(widths[:n//2] < width_median * max_stem_width)
+        # curve_length_wo_stem = cumulative_lengths[-1] - cumulative_lengths[stem_idx]
+        curve_length_wo_stem = curve_length - cumulative_lengths[stem_idx]
+        profiles = {'curve_points': curve_points,
+                    'normal_vectors': normal_vectors,
+                    'parameter_values': t,
+                    'curve_length': curve_length,
+                    'tck': tck,
+                    'method': parameterized_method,
+                    'normal_lines': normal_lines,
+                    'widths': widths,
+                    'indices': indices,
+                    'width_median': width_median,
+                    'abs_peak_threshold': peak_thresh * width_median,
+                    'cumulative_lengths': cumulative_lengths,
+                    'abs_change_rate': abs_change_rate,
+                    'peak_idx': peak_idx,
+                    'curve_length (remove stem)': curve_length_wo_stem,
+                    'stem_idx': stem_idx
+                    }
+        return profiles
+
+    @staticmethod
+    def plot_chart(profile, save_loc='plot.png', conversion_scale=1, unit='pixels'):
+        max_y = np.max(profile['widths']) * conversion_scale
+        max_x = np.max(profile['cumulative_lengths']) * conversion_scale
+        plt.plot(profile['cumulative_lengths'] * conversion_scale,
+                 profile['widths'] * conversion_scale,
+                 label='width', color='#1f77b4')
+        plt.plot(profile['cumulative_lengths'] * conversion_scale,
+                 profile['abs_change_rate'] * conversion_scale,
+                 label='abs. change rate', color='#ff7f0e')
+        plt.axhline(y=profile['width_median'] * conversion_scale, color='magenta', linestyle='--', label="width median")
+        plt.text(x=max_x / 2,
+                 y=profile['width_median'] * conversion_scale + 0.02 * max_y,
+                 s=round(profile['width_median'] * conversion_scale, 2),
+                 color='magenta')
+
+        # peak thresholding
+        plt.axhline(y=profile['abs_peak_threshold'] * conversion_scale, color='r', linestyle='--', label="peak thresh.")
+        plt.text(x=max_x / 2,
+                 y=profile['abs_peak_threshold'] * conversion_scale + 0.02 * max_y,
+                 s=round(profile['abs_peak_threshold'] * conversion_scale, 2),
+                 color='r')
+        peak_idx = profile['peak_idx']
+        stem_idx = profile['stem_idx']
+        plt.axvline(x=profile['cumulative_lengths'][peak_idx] * conversion_scale,
+                    color='gray', linestyle='--')
+        plt.axhline(y=profile['abs_change_rate'][peak_idx] * conversion_scale, color='#ff7f0e', linestyle='--')
+        plt.plot(profile['cumulative_lengths'][peak_idx] * conversion_scale,
+                 profile['abs_change_rate'][peak_idx] * conversion_scale, '^', color='#ff7f0e',
+                 label='peak change rate')
+        plt.text(
+                 # x=profile['cumulative_lengths'][idx] * conversion_scale,
+                 x=max_x / 2,
+                 y=profile['abs_change_rate'][peak_idx] * conversion_scale + 0.02 * max_y,
+                 s=round(profile['abs_change_rate'][peak_idx] * conversion_scale, 2),
+                 color='#ff7f0e')
+
+        # plt.axhline(y=profile['widths'][stem_idx] * conversion_scale, color='#1f77b4', linestyle='--')
+        plt.plot(profile['cumulative_lengths'][stem_idx] * conversion_scale,
+                 profile['widths'][stem_idx] * conversion_scale, 'o', color='#1f77b4',
+                 label='stem point')
+        plt.text(
+            # x=max_x / 2,
+            x=profile['cumulative_lengths'][stem_idx] * conversion_scale + 0.02 * max_x,
+            # y=profile['widths'][stem_idx] * conversion_scale + 0.02 * max_y,
+            y=profile['widths'][stem_idx] * conversion_scale,
+            s=(round(profile['cumulative_lengths'][stem_idx] * conversion_scale, 2), round(profile['widths'][stem_idx] * conversion_scale, 2)),
+            color='#1f77b4')
+
+        plt.xlabel('cumulative length ({})'.format(unit))
+        plt.ylabel('{}'.format(unit))
+        plt.tight_layout(pad=0)
+        plt.legend(bbox_to_anchor=(0.8, -0.1), ncol=2, fancybox=True, shadow=True)
+        plt.xlim(0, max_x)
+        plt.ylim(0, max_y)
+        plt.savefig(save_loc, bbox_inches="tight")
+        plt.clf()
---
+++ test_medial_axis.py
@@ -0,0 +1,95 @@
+import pathlib
+import cv2
+import numpy as np
+from PIL import Image
+from libs.medial_axis_analysis import CucumberShape
+from libs.vis_utils import draw_point, draw_line, draw_polygon, draw_text
+from matplotlib import pyplot as plt
+
+
+def plot_chart(curve_length, widths, save_loc='plot.png'):
+    gradient_width = np.zeros(widths.shape[0])
+    gradient_width[1:] = np.abs(widths[1:] - widths[0:-1])
+    plt.plot(curve_length, widths, label='width (cm)')
+    plt.plot(curve_length, gradient_width, label='width gradient (cm)')
+    plt.xlabel('cumulative length (cm)')
+    plt.ylabel('cm')
+    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
+    plt.legend()
+    plt.savefig(save_loc)
+
+
+def visualize(mask_file):
+    binary_mask_file = pathlib.Path(mask_file)
+    chart_and_gif_files_prefix = 'width_profile_'
+    stem_point_prefix = 'stem_point_'
+
+    binary_mask = cv2.imread(str(binary_mask_file), cv2.IMREAD_GRAYSCALE)
+    analyzer = CucumberShape()
+    tck, _, _, boundary = analyzer.find_medial_axis(binary_mask)
+    cm_per_pixel = 0.1048
+    num_samples = 200
+
+    # calculate width and remove stem
+    profiles = analyzer.width_profile(tck, boundary, num_samples=num_samples)
+    curve_points = profiles['curve_points']
+    analyzer.plot_chart(profiles, conversion_scale=cm_per_pixel, unit='cm',
+                        save_loc=binary_mask_file.parent / (chart_and_gif_files_prefix + binary_mask_file.with_suffix('.pdf').name))
+
+    # Define the desired height and width
+    h, w = binary_mask.shape
+    # Create an empty image
+    image = Image.new("RGB", (w, h))
+    draw_polygon(image, boundary, fill_color=0)
+    draw_line(image, curve_points)
+
+    image_gif = []
+    for normal_line, cp, length in zip(profiles['normal_lines'], profiles['curve_points'], profiles['cumulative_lengths']):
+        clone = image.copy()
+        draw_line(clone, normal_line, color=(0, 255, 255))
+        # p1, p2 = normal_line.coords
+        draw_point(clone, cp)
+        # draw_point(clone, p1)
+        # draw_point(clone, p2)
+        draw_text(clone, (10, 10), 'width: {:.2f} cm'.format(normal_line.length * cm_per_pixel), font_size=36)
+        draw_text(clone, (10, 50), 'length: {:.2f} cm'.format(length * cm_per_pixel), font_size=36)
+        image_gif.append(clone)
+    image_gif[0].save(binary_mask_file.parent / (chart_and_gif_files_prefix + binary_mask_file.with_suffix('.gif').name),
+                      save_all=True, append_images=image_gif[1:], optimize=False,
+                      duration=10, loop=0)
+    peak_img = image_gif[profiles['peak_idx']]
+    peak_img.save(binary_mask_file.parent / (stem_point_prefix + binary_mask_file.with_suffix('.png').name))
+
+    print('Length with stem (cm):', profiles['curve_length'] * cm_per_pixel)
+    print('Length w/o stem (cm):', profiles['curve_length (remove stem)'] * cm_per_pixel)
+    print('median width (cm):', profiles['width_median'] * cm_per_pixel)
+
+def main():
+    # File paths
+    file_list = [
+        # 'long_narrow_stem/warped_8/1.png',
+        # 'long_narrow_stem/warped_8/2.png',
+        # 'long_narrow_stem/warped_8/3.png',
+        # 'short_thick_stem/warped_1/1.png',
+        # 'short_thick_stem/warped_1/2.png',
+        # 'short_thick_stem/warped_1/3.png',
+        # 'no_stem/warped_11/0.png',
+        # 'no_stem/warped_11/1.png',
+        # 'no_stem/warped_11/2.png',
+        # 'no_stem/warped_11/3.png',
+        # 'no_stem/warped_11/4.png',
+        # 'no_stem/warped_17/0.png',
+        # 'no_stem/warped_17/1.png',
+        # 'no_stem/warped_17/3.png',
+        'no_stem/warped_17/4.png',
+        'no_stem/warped_17/5.png'
+    ]
+
+    for f in file_list:
+        print('Processing mask file at:', f)
+        visualize(f)
+        print('############################\n')
+
+
+if __name__ == '__main__':
+    main()
